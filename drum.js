96a523447a11b19d39f16d1a82d0731964a4c55b
Vivek20500 <141113863+Vivek20500@users.noreply.github.com>
2024-03-24 17:34:08 +0530

    Update drum.js

--- drum.js
+++ drum.js
@@ -9,30 +9,37 @@ function beat(){
         case "S":
             var sound=new Audio("crash.mp3")
             sound.play();
+            anim(inner);
             break;
         case "D":
             var sound=new Audio("tom-3.mp3")
             sound.play();
+            anim(inner);
             break;
         case "F":
             var sound=new Audio("tom-1.mp3")
             sound.play();
+            anim(inner);
             break;
         case "G":
             var sound=new Audio("kick-bass.mp3")
             sound.play();
+            anim(inner);
             break;
         case "H":
             var sound=new Audio("tom-2.mp3")
             sound.play();
+            anim(inner);
             break;
         case "J":
             var sound=new Audio("tom-4.mp3")
             sound.play();
+            anim(inner);
             break;
         case "K":
             var sound=new Audio("snare.mp3")
             sound.play();
+            anim(inner);
             break;
     }
 }
@@ -42,30 +49,50 @@ function keyb(e){
         case "KeyS":
             var sound=new Audio("crash.mp3")
             sound.play();
+            animkey(e);
             break;
         case "KeyD":
             var sound=new Audio("tom-3.mp3")
             sound.play();
+            animkey(e);
             break;
         case "KeyF":
             var sound=new Audio("tom-1.mp3")
             sound.play();
+            animkey(e);
             break;
         case "KeyG":
             var sound=new Audio("kick-bass.mp3")
             sound.play();
+            animkey(e);
             break;
         case "KeyH":
             var sound=new Audio("tom-2.mp3")
             sound.play();
+            animkey(e);
             break;
         case "KeyJ":
             var sound=new Audio("tom-4.mp3")
             sound.play();
+            animkey(e);
             break;
         case "KeyK":
             var sound=new Audio("snare.mp3")
             sound.play();
+            animkey(e);
             break;
     }
+}
+
+function anim(keey){
+    document.querySelector("."+keey).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("." + keey).classList.remove("pressed")},100);
+}
+
+function animkey(e){
+    var cap=e.key.toUpperCase();
+    document.querySelector('.'+cap).classList.add("pressed");
+    setTimeout(function() {
+        document.querySelector("."+cap).classList.remove("pressed")},100);
 }
